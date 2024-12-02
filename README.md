2207c6287eba2db0b9c7f9132ead56d242f937b7
Kurt Kanaskie <kurtkanaskie@google.com>
2024-12-02 18:39:47 -0500

    Substantive commit

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# apigee-edge-facade-v1
-Facade API proxy for Apigee Edge API to retrieve encrypted KVM entries.
+# Apigee Edge Facade API
+This Apigee Edge API proxy (edge-facade-v1) is a facade on the Apigee Edge Management API to enable retrieval of encrypted Key Value Map (KVM) entries.
+It can be used to facilitate migration from Edge to X when the source of the encrypted KVM entries is not available.
+If uses the same [KVM API paths](https://apidocs.apigee.com/docs/key-value-maps/1/overview) as the Edge API for 
+[Organziation KVM Entries Get](https://apidocs.apigee.com/docs/key-value-maps/1/routes/organizations/%7Borg_name%7D/apis/%7Bapi_name%7D/keyvaluemaps/%7Bmap_name%7D/entries/%7Bentry_name%7D/get)
+ and [Environment KVM Entries Get](https://apidocs.apigee.com/docs/key-value-maps/1/routes/organizations/%7Borg_name%7D/environments/%7Benv_name%7D/keyvaluemaps/%7Bmap_name%7D/entries/%7Bentry_name%7D/get).
+ Since the API is a front end to the Edge API, the proxy only needs to be deployed to one environment.
+
+ > **NOTE:** This example exploits an undocumented Edge Management API feature that can modify an existing policy in a deployed proxy. 
+>
+> This example uses a Service Callout to modify the currently deployed revision of itself to re-write the `mapIdentifier` attribute in the KVM policy prior to executing the policy.
+ It gets the name for the `mapIdentifier` as well as the KVM entry key name from the API path segments.
+
+ A bash script [kvms-export.sh](kvms-export.sh) is provided that exports all of the organization environment KVMs in a format that is suitable for import into an Apigee X organization using [apigeecli](https://github.com/apigee/apigeecli).
+
+
+## Use Case
+As an Apigee Edge platform owner migrating to Apigee X,\
+I need to retrieve private KVM entries,\
+So I can import them into X.
+
+## References
+[Working with key value maps | Apigee Edge](https://docs.apigee.com/api-platform/cache/key-value-maps)\
+[Working with key value maps | Apigee Edge - retrieving values](https://docs.apigee.com/api-platform/cache/key-value-maps#managingandusingkvms-retrievingkvms)\
+[Key value maps API - Apigee Edge APIs](https://apidocs.apigee.com/content/keyvalue-maps-management-api)\
+[get /organizations/{org_name}/keyvaluemaps/{map_name}/entries/{entry_name}](https://apidocs.apigee.com/docs/key-value-maps/1/routes/organizations/%7Borg_name%7D/keyvaluemaps/%7Bmap_name%7D/entries/%7Bentry_name%7D/get)\
+[get /organizations/{org_name}/environments/{env_name}/keyvaluemaps/{map_name}\entries/{entry_name}](https://apidocs.apigee.com/docs/key-value-maps/1/routes/organizations/%7Borg_name%7D/environments/%7Benv_name%7D/keyvaluemaps/%7Bmap_name%7D/entries/%7Bentry_name%7D/get)
+
+## Issue
+Apigee Edge management APIs do not support retrieval of encrypted KVM entries.\
+The only way to retrieve encrypted KVM entries is by using a KVM policy.\
+The KVM policy `mapIdentifier` attribute requires a static value and cannot be paramaterized.
+
+## Limitations
+This “technique” only works for organization and environment scoped KVMs.
+API proxy scoped KVMs can only be accessed by the proxy in which they are deployed.
+
+# Solution Architecture
+![API Proxy](proxy-overview.png)
+
+## Overview
+Create an Edge KVM entries helper proxy that mimics the Management API for KVM entries (edge-facade-v1)\
+Configure the proxy to respond to:
+* GET /organizations/*/keyvaluemaps/*/entries/*
+* GET /organizations/*/environments/*/keyvaluemaps/*/entries/*
+
+Exploit an undocumented ability to modify an existing policy in a deployed proxy
+* Use Service Callouts in the proxy flow to rewrite the KVM policy to change the mapIdentifier attribute and Scope element on the fly
+  * Control the Service Callout using a condition for subsequent retrieval of entries in the same KVM
+* Use KVM policy to get the entry value in the “private.value” variable
+* Use Assign Message policy to return the unencrypted value
+
+
+## Deployment
+Deploy the `edge-facade-v1` proxy to the Edge organization.
+```
+zip -r edge-facade-v1.zip apiproxy
+```
+Import into Edge using UI and deploy.
+
+
+
+## Test Flow
+```
+Call Edge API to retrieve org level KVMs
+export ORG=your_edge_org
+export HOST=your_edge_org-your_env.apigee.net
+B64UNPW=$(echo -n 'username@gmail.com:secret123' | base64)
+AUTH="Authorization:Basic $B64UNPW"
+
+curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps | jq
+[
+  "org-config",
+  "org-config-private"
+]
+```
+
+Call Edge API to retrieve KVM entries
+```
+curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/org-config-private/keys | jq
+[
+  "private_key1",
+  "private_key2"
+]
+```
+
+
+Call Edge Facade API proxy to retrieve KVM entries
+```
+# First call instructs proxy to modify the proxy KVM policy using Service Callout by passing callout query param set to true
+curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/org-config-private/entries/private_key1?callout=true | jq
+{
+  "name": "private_key1",
+  "value": "private_value1"
+}
+
+# Subsequent calls use the currently configured KVM policy
+curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/org-config-private/entries/private_key2 | jq
+{
+  "name": "private_key2",
+  "value": "private_value2"
+}
+```
+
---
+++ kvms-export.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Works on GNU bash, version 3.2.57(1)-release (x86_64-apple-darwin20)
+# Author: Kurt Kanaskie, Google (kurtkanaskie@google.com)
+# Updated: 2024-12-02
+
+Help()
+{
+   # Display Help
+   echo
+   echo "Export KVMs and entries including encrypted entries."
+   echo "Output format for Edge or X (suitable for import to X with apigeecli)"
+   echo "Uses environment variables for ORG, ENVS and B64UNPW if set."
+   echo
+   echo "Syntax: $0 [-h|-e|-o|-b|-f|-x"
+   echo "options:"
+   echo "    -h    Print this help"
+   echo "    -b    Base 64 encoded username:password"
+   echo "    -e    Environment name(s) space separated, if empty all envronments are exported"
+   echo "    -f    Output format [ edge | X (default) ]"
+   echo "    -o    Organization name"
+   echo "    -x    Export output dir"
+   echo
+   echo "Usage:      $0 -b B64UNPW -o ORG, and -x OUTPUT_DIR options or environment variables are required."
+   echo
+   echo "Usage X:    $0 -x edge-export -f Edge"
+   echo "Usage Edge: $0 -x x-import -f X"
+
+   echo
+}
+
+while getopts "b:e:f:o:x:h" flag
+do
+    case "${flag}" in
+        h) Help
+            exit;;
+        e) ENVS=${OPTARG};;
+        o) ORG=${OPTARG};;
+        b) B64UNPW=${OPTARG};;
+        f) FORMAT=${OPTARG};;
+        x) OUTPUT_DIR=${OPTARG};;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            Help
+        exit;;
+    esac
+done
+
+if [[ "$ORG" == "" || "$OUTPUT_DIR" == "" || "$B64UNPW" == "" ]]
+then
+    echo
+    echo "ERROR: -b B64UNPW -o ORG, and -x OUTPUT_DIR options or environment variables are required."
+    Help
+    exit
+fi
+
+# Psuedo code
+: '
+mkdir -p $OUTPUT_DIR
+GET KVMs for org
+  for each KVM
+    GET KVM to check for encrpted 
+    GET KEMV keys
+    GET first key by rewriting the KV-decrypt-entry policy for the org and map name
+    GET subsequent key values without rewriting the KV-decrypt-entry policy
+'
+
+# Only difference between Edge and X output format
+if [[ "$FORMAT" == "" || "$FORMAT" == "X" ]]; then
+  FORMAT="X"
+  ENTRIES_NAME=keyValueEntries
+  if [[ ! -d "${OUTPUT_DIR}" ]]; then
+    mkdir -p ${OUTPUT_DIR}
+  fi
+else
+  FORMAT="Edge"
+  ENTRIES_NAME=entry
+  if [[ ! -d "${OUTPUT_DIR}/kvm/org" ]]; then
+    mkdir -p ${OUTPUT_DIR}/kvm/org
+  fi
+fi
+
+AUTH="Authorization:Basic $B64UNPW"
+
+# Get the list of org KVMs
+KVMS=$(curl -s -H "$AUTH" -s https://${HOST}/edge-facade/v1/organizations/${ORG}/keyvaluemaps | jq -r .[])
+if [[ "$KVMS" == *'"error":'* ]]; then
+  echo
+  echo "ERROR: unauthorized."
+  echo "MESSAGE: $KVMS"
+  Help
+  exit
+fi
+
+##################################################
+# Organization
+
+echo Organization KVMs: $ORG
+for KVM in ${KVMS}
+do
+  # Exclude KVMS: CustomReports* privacy
+  if [[ "$KVM" == *"CustomReports"* || "$KVM" == "privacy" ]]; then
+    echo "Skipping $KVM"
+  else
+    echo "Exporting organization $KVM"
+    
+    # Get encryption
+    ENCRYPTED=$(curl -s -H "$AUTH" -s https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/$KVM | jq .encrypted)
+    
+    # Get the list of keys
+    KEYS=$(curl -s -H "$AUTH" -s https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/$KVM/keys | jq -r .[])
+    
+    RESPONSE='{"encrypted":'"${ENCRYPTED}"',"name":"'"${KVM}"'","'"${ENTRIES_NAME}"'":['
+    FIRST=1
+    for KEY in ${KEYS}
+    do
+      if [ "$FIRST" -eq 1 ]; then
+        # invoke the Service Callout to modify the proxy KVM policy by passing callout query param set to true
+        ENTRY=$(curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/$KVM/entries/$KEY?callout=true)
+        RESPONSE="${RESPONSE}${ENTRY}"
+        FIRST=0
+      else
+        # Retrieve other values from the same KVM without modifying the proxy KVM policy
+        ENTRY=$(curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/$KVM/entries/$KEY?callout=false)
+        RESPONSE="${RESPONSE}, ${ENTRY}"
+      fi
+    done
+    RESPONSE="$RESPONSE"']}'
+
+    echo $RESPONSE | jq 
+    if [[ "$FORMAT" == "X" ]]; then
+      echo $RESPONSE | jq > ${OUTPUT_DIR}/org__${KVM}__kvmfile__0.json
+    else
+      echo $RESPONSE | jq > $OUTPUT_DIR/kvm/org/$KVM
+    fi
+  fi
+done
+
+##################################################
+# Environments
+
+# If no ENVS passed, get all ENVS
+if [[ "$ENVS" == "" ]]
+then
+    ENVS=$(curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/environments | jq .[] -r)
+fi
+
+for E in $ENVS
+do
+  echo Environment KVMs: $E
+  KVMS=$(curl -s -H "$AUTH" -s https://${HOST}/edge-facade/v1/organizations/${ORG}/environments/${E}/keyvaluemaps | jq -r .[])
+  for KVM in ${KVMS}
+  do
+    echo "Exporting Environment $E $KVM"
+    
+    # Get encryption
+    ENCRYPTED=$(curl -s -H "$AUTH" -s https://$HOST/edge-facade/v1/organizations/$ORG/environments/${E}/keyvaluemaps/$KVM | jq .encrypted)
+    
+    # Get the list of keys
+    KEYS=$(curl -s -H "$AUTH" -s https://$HOST/edge-facade/v1/organizations/$ORG/environments/${E}/keyvaluemaps/$KVM/keys | jq -r .[])
+    
+    RESPONSE='{"encrypted":'"${ENCRYPTED}"',"name":"'"${KVM}"'","'"${ENTRIES_NAME}"'":['
+    FIRST=1
+    for KEY in ${KEYS}
+    do
+      if [ "$FIRST" -eq 1 ]; then
+        # invoke the Service Callout to modify the proxy KVM policy by passing callout query param set to true
+        ENTRY=$(curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/environments/${E}/keyvaluemaps/$KVM/entries/$KEY?callout=true)
+        RESPONSE="${RESPONSE}${ENTRY}"
+        FIRST=0
+      else
+        # Retrieve other values from the same KVM without modifying the proxy KVM policy
+        ENTRY=$(curl -s -H "$AUTH" https://$HOST/edge-facade/v1/organizations/$ORG/environments/${E}/keyvaluemaps/$KVM/entries/$KEY?callout=false)
+        RESPONSE="${RESPONSE}, ${ENTRY}"
+      fi
+    done
+    RESPONSE="$RESPONSE"']}'
+
+    echo $RESPONSE | jq 
+    if [[ "$FORMAT" == "X" ]]; then
+      echo $RESPONSE | jq > ${OUTPUT_DIR}/env__${E}__${KVM}__kvmfile__0.json
+    else
+      if [[ ! -d "${OUTPUT_DIR}/kvm/env/${E}" ]]; then
+        mkdir -p ${OUTPUT_DIR}/kvm/env/${E}
+      fi
+      echo $RESPONSE | jq > ${OUTPUT_DIR}/kvm/env/${E}/$KVM
+    fi
+  done
+done
+
+##################################################
+# Update KVM policy to default, not-found KVM
+echo Updating KVM policy to \"not-found\" KVM
+curlb https://$HOST/edge-facade/v1/organizations/$ORG/keyvaluemaps/not-found/entries/entry?callout=true | jq
+
